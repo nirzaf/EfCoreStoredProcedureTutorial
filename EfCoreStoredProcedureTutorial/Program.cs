93467f84c2165399af9349c05ab445a992dcb1bb
nirzaf <mfmfazrin1986@gmail.com>
2024-02-01 22:20:01 +0300

    Remove EF Core StoredProcedureTutorial components
    Deleted several files related to the StoredProcedureTutorial, including `Character.cs`, `CharacterController.cs`, `dotnetrpgContext.cs`, `Skill.cs`, `User.cs`, and `Weapon.cs`. Also commented out the Database Context service registration in `Program.cs`. This suggests a shift away from the tutorial design, potentially to restructure or reimplement these parts of the application.

--- EfCoreStoredProcedureTutorial/Program.cs
+++ EfCoreStoredProcedureTutorial/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<dotnetrpgContext>();
+//builder.Services.AddDbContext<dotnetrpgContext>();
 
 var app = builder.Build();
 
